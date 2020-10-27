6102a148ccaa3da3dfb6789fd7017406adab8f37
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2020-10-27 11:03:20 +0200

    dz first part

---
+++ ORM-EF-core/ORM-EF-core/Migrations/20201024192812_InitialCreate.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ORM_EF_core.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Faculties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faculties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 255, nullable: false),
+                    FacultyId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Groups_Faculties_FacultyId",
+                        column: x => x.FacultyId,
+                        principalTable: "Faculties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 255, nullable: false),
+                    GroupId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_FacultyId",
+                table: "Groups",
+                column: "FacultyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_GroupId",
+                table: "Students",
+                column: "GroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Faculties");
+        }
+    }
+}
---
+++ ORM-EF-core/ORM-EF-core/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ORM_EF_core
+{
+    class Faculty
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; }
+        public ICollection<Group> Groups { get; set; }
+    }
+    class Group
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; }
+        public ICollection<Student> Students { get; set; }
+        public Faculty Faculty { get; set; }
+    }
+    class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; }
+        public Group Group { get; set; }
+    }
+    class UniversityContext : DbContext
+    {
+        public UniversityContext()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-IRISS61;Database=UniversityDataBase;Trusted_Connection=True");
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Faculty> Faculties { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Group>()
+                .HasMany(g => g.Students)
+                .WithOne(s => s.Group);
+
+            modelBuilder.Entity<Faculty>()
+                .HasMany(f => f.Groups)
+                .WithOne(g => g.Faculty);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
